323c9c9e8d75f442f570a196956156a168e032b9
aathmajleo <aathmaj6@gmail.com>
2025-08-09 01:05:25 +0530

    lotta bugs but cant afford to lose this one

---
+++ test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import StringVar
+
+# Create the main window
+root = tk.Tk()
+root.title("Manual Manual Clock")
+root.configure(bg="black")
+
+
+
+root.mainloop()
